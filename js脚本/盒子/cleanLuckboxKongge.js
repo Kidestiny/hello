42038b7211608162a618c37ff0f15598daffe7cb
Kidestiny <954137343@qq.com>
2021-08-26 18:44:11 +0800

    增加了js脚本备份

---
+++ js脚本/盒子/cleanLuckboxKongge.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+
+var str=fs.readFileSync('./luckbox.js','utf8');
+str = str.replace(/\ +/g,"");   
+       
+str = str.replace(/[\r\n]/g,"");        
+console.log(str);
+
+fs.writeFile('./luckboxTest.js',str,(error)=>{
+    if(error){
+        console.log('wrong');
+    }else{
+        console.log('succeed');
+    }
+})
---
+++ js脚本/盒子/dreamHallData - 副本 - 副本.js
@@ -0,0 +1,827 @@
+dreamHallData = [
+    
+
+    {
+        "name": "鸩杀少帝-李儒",
+        "data": "zs_lr",
+        "chance": "zs_lr_chance",
+        "src":"https://sgs-7gp2uaju8f978987-1304922515.tcloudbaseapp.com/dreamHallPic/liru.jpg?sign=cef08a10c9dc8b74251168ed2c5edb5a&t=1619785391",
+    }, 
+    {
+        "name": "登锋陷阵-张辽",
+        "data": "df_zl",
+        "chance": "df_zl_chance",
+        "src":"https://sgs-7gp2uaju8f978987-1304922515.tcloudbaseapp.com/dreamHallPic/zhangliao.jpg?sign=838d50bc3cfce4971d4a7fec4bf3af19&t=1618568640",
+    },
+    {
+        "name": "威严毅重-于禁",
+        "data": "wy_yj",
+        "chance": "wy_yj_chance",
+        "src":"https://sgs-7gp2uaju8f978987-1304922515.tcloudbaseapp.com/dreamHallPic/yujin.jpg?sign=f022bbd499512ab1fa72a827e0f7d3bf&t=1617454536",
+    },
+    {
+        "name": "鏖战赤壁-黄盖",
+        "data": "az_hg",
+        "chance": "az_hg_chance",
+        "src":"https://sgs-7gp2uaju8f978987-1304922515.tcloudbaseapp.com/dreamHallPic/huanggai.jpg?sign=b22266a78c5acc32e78d4e2b48bb0bc9&t=1617454574",
+    },
+    {
+        "name": "万人辟易-神甘宁",
+        "data": "wr_sgn",
+        "chance": "wr_sgn_chance",
+        "src":"https://sgs-7gp2uaju8f978987-1304922515.tcloudbaseapp.com/dreamHallPic/shenganning.jpg?sign=51c2b2332d1b9c3ce0f6e584d8b97fdd&t=1617454565",
+    },
+    {
+        "name": "如花似朵-小乔",
+        "data": "rh_xq",
+        "chance": "rh_xq_chance",
+        "src":"https://sgs-7gp2uaju8f978987-1304922515.tcloudbaseapp.com/dreamHallPic/xiaoqiao.jpg?sign=7898a9647cd75019247acf4b0afd41ac&t=1617454556",
+    },
+    {
+        "name": "盛气凌人-许攸",
+        "data": "sq_xy",
+        "chance": "sq_xy_chance",
+        "src":"https://sgs-7gp2uaju8f978987-1304922515.tcloudbaseapp.com/dreamHallPic/xuyou.jpg?sign=552ef935150c3290adee17c852fcdc5c&t=1617454547",
+    },
+    {
+        "name": "笼中箜响-周妃",
+        "data": "lz_zf",
+        "chance": "lz_zf_chance",
+        "src":"https://sgs-7gp2uaju8f978987-1304922515.tcloudbaseapp.com/dreamHallPic/zhoufei.jpg?sign=6fcf7a303d14ee6d39b98986dfe73db3&t=1617454497",
+    },
+    {
+        "name": "啸风从龙*关羽",
+        "data": "xf_gy",
+        "chance": "xf_gy_chance",
+        "src":"https://sgs-7gp2uaju8f978987-1304922515.tcloudbaseapp.com/dreamHallPic/guanyu.jpg?sign=2e3efdbaa3badb005668bfadeb5dcda5&t=1617454583",
+    }
+]
+
+skinData = {
+    
+    "zs_lr": [{
+        "chance": 9.495,
+        "name": "菜篮子*2",
+        "number": 2
+    },
+    {
+        "chance": 15,
+        "name": "点将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "换将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "手气卡*2",
+        "number": 2
+    },
+    {
+        "chance": 10,
+        "name": "进阶丹*1",
+        "number": 1
+    },
+    {
+        "chance": 10,
+        "name": "雁翎甲",
+        "number": 1
+    },
+    {
+        "chance": 6,
+        "name": "招募令",
+        "number": 1
+    },
+    {
+        "chance": 4,
+        "name": "史诗宝珠碎片*1",
+        "number": 1
+    },
+    {
+        "chance": 5,
+        "name": "欢乐豆*50",
+        "number": 50
+    },
+    {
+        "chance": 0.5,
+        "name": "史诗宝珠*1",
+        "number": 1
+    },
+    {
+        "chance": 0.005,
+        "name": "🐏徽瑜",
+        "number": 1,
+        "power": 500
+    }
+    ],
+
+    "df_zl": [{
+        "chance": 9.495,
+        "name": "菜篮子*2",
+        "number": 2
+    },
+    {
+        "chance": 15,
+        "name": "点将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "换将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "手气卡*2",
+        "number": 2
+    },
+    {
+        "chance": 10,
+        "name": "进阶丹*1",
+        "number": 1
+    },
+    {
+        "chance": 10,
+        "name": "雁翎甲",
+        "number": 1
+    },
+    {
+        "chance": 6,
+        "name": "招募令",
+        "number": 1
+    },
+    {
+        "chance": 4,
+        "name": "史诗宝珠碎片*1",
+        "number": 1
+    },
+    {
+        "chance": 5,
+        "name": "欢乐豆*50",
+        "number": 50
+    },
+    {
+        "chance": 0.5,
+        "name": "史诗宝珠*1",
+        "number": 1
+    },
+    {
+        "chance": 0.005,
+        "name": "🐏徽瑜",
+        "number": 1,
+        "power": 500
+    }
+    ],
+
+    "wy_yj": [{
+        "chance": 9.495,
+        "name": "菜篮子*2",
+        "number": 2
+    },
+    {
+        "chance": 15,
+        "name": "点将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "换将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "手气卡*2",
+        "number": 2
+    },
+    {
+        "chance": 10,
+        "name": "进阶丹*1",
+        "number": 1
+    },
+    {
+        "chance": 10,
+        "name": "雁翎甲",
+        "number": 1
+    },
+    {
+        "chance": 6,
+        "name": "招募令",
+        "number": 1
+    },
+    {
+        "chance": 4,
+        "name": "史诗宝珠碎片*1",
+        "number": 1
+    },
+    {
+        "chance": 5,
+        "name": "欢乐豆*50",
+        "number": 50
+    },
+    {
+        "chance": 0.5,
+        "name": "史诗宝珠*1",
+        "number": 1
+    },
+    {
+        "chance": 0.005,
+        "name": "🐏徽瑜",
+        "number": 1,
+        "power": 500
+    }
+    ],
+    "az_hg": [{
+        "chance": 9.495,
+        "name": "菜篮子*2",
+        "number": 2
+    },
+    {
+        "chance": 15,
+        "name": "点将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "换将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "手气卡*2",
+        "number": 2
+    },
+    {
+        "chance": 10,
+        "name": "进阶丹*1",
+        "number": 1
+    },
+    {
+        "chance": 10,
+        "name": "雁翎甲",
+        "number": 1
+    },
+    {
+        "chance": 6,
+        "name": "招募令",
+        "number": 1
+    },
+    {
+        "chance": 4,
+        "name": "史诗宝珠碎片*1",
+        "number": 1
+    },
+    {
+        "chance": 5,
+        "name": "欢乐豆*50",
+        "number": 50
+    },
+    {
+        "chance": 0.5,
+        "name": "史诗宝珠*1",
+        "number": 1
+    },
+    {
+        "chance": 0.005,
+        "name": "史诗宝珠*66",
+        "number": 1,
+        "power": 500
+    }
+    ],
+    "wr_sgn": [{
+        "chance": 9.495,
+        "name": "菜篮子*2",
+        "number": 2
+    },
+    {
+        "chance": 15,
+        "name": "点将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "换将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "手气卡*2",
+        "number": 2
+    },
+    {
+        "chance": 10,
+        "name": "进阶丹*1",
+        "number": 1
+    },
+    {
+        "chance": 10,
+        "name": "雁翎甲",
+        "number": 1
+    },
+    {
+        "chance": 6,
+        "name": "招募令",
+        "number": 1
+    },
+    {
+        "chance": 4,
+        "name": "史诗宝珠碎片*1",
+        "number": 1
+    },
+    {
+        "chance": 5,
+        "name": "欢乐豆*50",
+        "number": 50
+    },
+    {
+        "chance": 0.5,
+        "name": "史诗宝珠*1",
+        "number": 1
+    },
+    {
+        "chance": 0.005,
+        "name": "神甘宁",
+        "number": 1,
+    }
+    ],
+    "rh_xq": [{
+        "chance": 9.495,
+        "name": "菜篮子*2",
+        "number": 2
+    },
+    {
+        "chance": 15,
+        "name": "点将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "换将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "手气卡*2",
+        "number": 2
+    },
+    {
+        "chance": 10,
+        "name": "进阶丹*1",
+        "number": 1
+    },
+    {
+        "chance": 10,
+        "name": "雁翎甲",
+        "number": 1
+    },
+    {
+        "chance": 6,
+        "name": "招募令",
+        "number": 1
+    },
+    {
+        "chance": 4,
+        "name": "史诗宝珠碎片*1",
+        "number": 1
+    },
+    {
+        "chance": 5,
+        "name": "欢乐豆*50",
+        "number": 50
+    },
+    {
+        "chance": 0.5,
+        "name": "史诗宝珠*1",
+        "number": 1
+    },
+    {
+        "chance": 0.005,
+        "name": "🐏徽瑜",
+        "number": 1,
+    }
+    ],
+    "sq_xy": [{
+        "chance": 9.495,
+        "name": "菜篮子*2",
+        "number": 2
+    },
+    {
+        "chance": 15,
+        "name": "点将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "换将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "手气卡*2",
+        "number": 2
+    },
+    {
+        "chance": 10,
+        "name": "进阶丹*1",
+        "number": 1
+    },
+    {
+        "chance": 10,
+        "name": "雁翎甲",
+        "number": 1
+    },
+    {
+        "chance": 6,
+        "name": "招募令",
+        "number": 1
+    },
+    {
+        "chance": 4,
+        "name": "史诗宝珠碎片*1",
+        "number": 1
+    },
+    {
+        "chance": 5,
+        "name": "欢乐豆*50",
+        "number": 50
+    },
+    {
+        "chance": 0.5,
+        "name": "史诗宝珠*1",
+        "number": 1
+    },
+    {
+        "chance": 0.005,
+        "name": "神甘宁",
+        "number": 1,
+    }
+    ],
+    "lz_zf":[{
+        "chance": 9.5,
+        "name": "菜篮子*2",
+        "number": 2
+    },
+    {
+        "chance": 15,
+        "name": "点将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "换将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "手气卡*2",
+        "number": 2
+    },
+    {
+        "chance": 10,
+        "name": "进阶丹*1",
+        "number": 1
+    },
+    {
+        "chance": 10,
+        "name": "雁翎甲",
+        "number": 1
+    },
+    {
+        "chance": 6,
+        "name": "招募令",
+        "number": 1
+    },
+    {
+        "chance": 4,
+        "name": "史诗宝珠碎片*1",
+        "number": 1
+    },
+    {
+        "chance": 5,
+        "name": "欢乐豆*50",
+        "number": 50
+    },
+    {
+        "chance": 0.5,
+        "name": "史诗宝珠*1",
+        "number": 1
+    }
+    ],
+    "xf_gy":[{
+        "chance": 9.5,
+        "name": "菜篮子*2",
+        "number": 2
+    },
+    {
+        "chance": 15,
+        "name": "点将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "换将卡*2",
+        "number": 2
+    },
+    {
+        "chance": 20,
+        "name": "手气卡*2",
+        "number": 2
+    },
+    {
+        "chance": 10,
+        "name": "进阶丹*1",
+        "number": 1
+    },
+    {
+        "chance": 10,
+        "name": "雁翎甲",
+        "number": 1
+    },
+    {
+        "chance": 6,
+        "name": "招募令",
+        "number": 1
+    },
+    {
+        "chance": 4,
+        "name": "史诗宝珠碎片*1",
+        "number": 1
+    },
+    {
+        "chance": 5,
+        "name": "欢乐豆*50",
+        "number": 50
+    },
+    {
+        "chance": 0.5,
+        "name": "史诗宝珠*1",
+        "number": 1
+    }
+    ],
+}
+
+newChipChance = {
+    "zs_lr_chance": [{
+        0: 100,
+        1: 96,
+        2: 92,
+        3: 88,
+        4: 84,
+        5: 80,
+        6: 76,
+        7: 72,
+        8: 68,
+        9: 64,
+        10: 42.86,
+        11: 40.0025,
+        12: 37.145,
+        13: 34.2875,
+        14: 31.43,
+        15: 25,
+        16: 22.5,
+        17: 20,
+        18: 17.5,
+        19: 15,
+        20: 7.62,
+        21: 6.0025,
+        22: 4.385,
+        23: 2.7675,
+        24: 1.15,
+    }],
+    "df_zl_chance": [{
+        0: 100,
+        1: 96,
+        2: 92,
+        3: 88,
+        4: 84,
+        5: 80,
+        6: 76,
+        7: 72,
+        8: 68,
+        9: 64,
+        10: 42.86,
+        11: 40.0025,
+        12: 37.145,
+        13: 34.2875,
+        14: 31.43,
+        15: 25,
+        16: 22.5,
+        17: 20,
+        18: 17.5,
+        19: 15,
+        20: 7.62,
+        21: 6.0025,
+        22: 4.385,
+        23: 2.7675,
+        24: 1.15,
+    }],
+    "wy_yj_chance": [{
+        0: 100,
+        1: 96,
+        2: 92,
+        3: 88,
+        4: 84,
+        5: 80,
+        6: 76,
+        7: 72,
+        8: 68,
+        9: 64,
+        10: 60,
+        11: 56,
+        12: 52,
+        13: 48,
+        14: 44,
+        15: 25,
+        16: 22.5,
+        17: 20,
+        18: 17.5,
+        19: 15,
+        20: 10.11,
+        21: 8.08,
+        22: 6.06,
+        23: 4.04,
+        24: 2.02,
+    }],
+    "az_hg_chance": [{
+        0: 100,
+        1: 96,
+        2: 92,
+        3: 88,
+        4: 84,
+        5: 80,
+        6: 76,
+        7: 72,
+        8: 68,
+        9: 64,
+        10: 60,
+        11: 56,
+        12: 52,
+        13: 48,
+        14: 44,
+        15: 40,
+        16: 36,
+        17: 32,
+        18: 28,
+        19: 24,
+        20: 20,
+        21: 16,
+        22: 12,
+        23: 8,
+        24: 3,
+    }],
+    "wr_sgn_chance": [{
+        0: 100,
+        1: 96,
+        2: 92,
+        3: 88,
+        4: 84,
+        5: 80,
+        6: 76,
+        7: 72,
+        8: 68,
+        9: 64,
+        10: 60,
+        11: 56,
+        12: 52,
+        13: 48,
+        14: 44,
+        15: 25,
+        16: 22.5,
+        17: 20,
+        18: 17.5,
+        19: 15,
+        20: 6.98,
+        21: 5.41,
+        22: 3.84,
+        23: 2.27,
+        24: 0.7,
+    }],
+    "rh_xq_chance": [{
+        0: 100,
+        1: 96,
+        2: 92,
+        3: 88,
+        4: 84,
+        5: 80,
+        6: 76,
+        7: 72,
+        8: 68,
+        9: 64,
+        10: 60,
+        11: 56,
+        12: 52,
+        13: 48,
+        14: 44,
+        15: 25,
+        16: 22.5,
+        17: 20,
+        18: 17.5,
+        19: 15,
+        20: 6.98,
+        21: 5.41,
+        22: 3.84,
+        23: 2.27,
+        24: 0.7,
+    }],
+    "sq_xy_chance": [{
+        0: 100,
+        1: 96,
+        2: 92,
+        3: 88,
+        4: 84,
+        5: 80,
+        6: 76,
+        7: 72,
+        8: 68,
+        9: 64,
+        10: 60,
+        11: 56,
+        12: 52,
+        13: 48,
+        14: 44,
+        15: 25,
+        16: 22,
+        17: 19,
+        18: 16,
+        19: 15,
+        20: 7.62,
+        21: 6.095,
+        22: 4.57,
+        23: 3.045,
+        24: 1.52,
+    }],
+    "lz_zf_chance": [{
+        0: 100,
+        1: 95,
+        2: 90,
+        3: 85,
+        4: 80,
+        5: 66.7,
+        6: 62.525,
+        7: 58.35,
+        8: 54.175,
+        9: 50,
+        10: 31,
+        11: 28.7,
+        12: 26.4,
+        13: 24.1,
+        14: 21.8,
+        15: 19.5,
+        16: 17.2,
+        17: 14.9,
+        18: 12.6,
+        19: 10.3,
+        20: 5,
+        21: 4.375,
+        22: 3.75,
+        23: 3.125,
+        24: 2.5,
+    }],
+    "xf_gy_chance": [{
+        0: 100,
+        1: 95,
+        2: 90,
+        3: 85,
+        4: 80,
+        5: 66.7,
+        6: 62.525,
+        7: 58.35,
+        8: 54.175,
+        9: 50,
+        10: 37.5,
+        11: 34.72,
+        12: 31.94,
+        13: 29.16,
+        14: 26.39,
+        15: 23.61,
+        16: 20.83,
+        17: 18.05,
+        18: 15.27,
+        19: 12.5,
+        20: 9,
+        21: 7.175,
+        22: 5.35,
+        23: 3.525,
+        24: 1.7,
+    }],
+}
+
+let a = [dreamHallData, skinData, newChipChance]
+let str = JSON.stringify(a);
+var fs = require('fs');
+
+// fs.writeFile('./dreanHallData.json', str, (error) => {
+//     if (error) {
+//         console.log('wrong');
+//     } else {
+//         console.log('succeed');
+//     }
+// })
+fs.writeFile('./dreanHallDataTest.json', str, (error) => {
+    if (error) {
+        console.log('wrong');
+    } else {
+        console.log('succeed');
+    }
+})
